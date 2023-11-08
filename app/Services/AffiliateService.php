aabf7c8f8f8134da66d6693173efcda542c2bc72
Naveed <engir.naveedshahzad@gmail.com>
2023-11-08 20:34:24 +0000

    AffiliateServiceTest has been completed

--- app/Services/AffiliateService.php
+++ app/Services/AffiliateService.php
@@ -28,5 +28,20 @@ class AffiliateService
     public function register(Merchant $merchant, string $email, string $name, float $commissionRate): Affiliate
     {
         // TODO: Complete this method
+        $user = User::whereEmail($email)->first();
+        if($user)
+            throw new AffiliateCreateException;
+
+        $user = User::create(['name'=>$name,'email'=>$email, 'type'=>User::TYPE_AFFILIATE, 'password'=>bcrypt("12345678")]);
+
+        $discountCode = $this->apiService->createDiscountCode($merchant);
+        $affiliate = $user->affiliate()->create([
+           'merchant_id' => $merchant->id,
+           'user_id' => $merchant->user->id,
+           'commission_rate'=> $commissionRate,
+           'discount_code'=> $discountCode['code'],
+        ]);
+        Mail::to($email)->send(new AffiliateCreated($affiliate));
+        return $affiliate;
     }
 }
